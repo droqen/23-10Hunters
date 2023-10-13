8e857b6e634a230c2435548430abf4b7eab7ff58
Alexander Martin <hi@droqen.com>
2023-10-13 01:03:31 -0400

    test 2 with orb. "flag" is a bad name for this pkg

--- demos/flagtester/src/server.rs
+++ demos/flagtester/src/server.rs
@@ -1,16 +1,35 @@
 use ambient_api::{
+    cb::Callback,
     core::{
         camera::concepts::{
             PerspectiveInfiniteReverseCamera, PerspectiveInfiniteReverseCameraOptional,
         },
-        primitives::components::{cube, quad},
-        transform::components::{lookat_target, translation},
+        messages::Frame,
+        primitives::{
+            components::{cube, quad},
+            concepts::Sphere,
+        },
+        transform::{
+            components::{lookat_target, translation},
+            concepts::Transformable,
+        },
     },
     prelude::*,
+};
+use packages::{
+    cubeline::components::{cubeline_a, cubeline_b, cubeline_width},
+    flag::components::{flaginertia, flaglerp, flaglin, flagpole, flagpole_target_offset},
 };
 
 #[main]
 pub fn main() {
+    spawn_cam();
+    // spawn_quad();
+    // spawn_test1_simple();
+    spawn_test2_orb_body();
+}
+
+fn spawn_cam() {
     PerspectiveInfiniteReverseCamera {
         optional: PerspectiveInfiniteReverseCameraOptional {
             aspect_ratio_from_window: Some(entity::resources()),
@@ -23,16 +42,16 @@ pub fn main() {
     .make()
     .with(lookat_target(), vec3(0., 0., 0.))
     .spawn();
+}
 
+fn spawn_quad() {
     Entity::new()
         .with(translation(), vec3(0., 0., 0.))
         .with(quad(), ())
         .spawn();
+}
 
-    // make a droplet with cubeline and flags
-    // todo
-
-    use packages::cubeline::components::{cubeline_a, cubeline_b, cubeline_width};
+fn spawn_test1_simple() {
     let tester_cline = Entity::new()
         .with(cube(), ())
         .with(cubeline_a(), vec3(0., 0., 5.))
@@ -43,7 +62,6 @@ pub fn main() {
         .with(cube(), ())
         .with(translation(), vec3(0., 0., 0.))
         .spawn();
-    use packages::flag::components::{flaginertia, flaglerp, flagpole};
     let wobbler_flag = Entity::new()
         .with(cube(), ())
         .with(flagpole(), wobbler)
@@ -61,4 +79,60 @@ pub fn main() {
             entity::get_component(wobbler, translation()).unwrap(),
         );
     });
+}
+
+fn spawn_test2_orb_body() {
+    let ball_goto = Entity::new().with(translation(), vec3(10., 0., 0.)).spawn();
+    let ball_head = make_ball(0.5)
+        .with(translation(), vec3(0., 0., 1.))
+        .with(flagpole(), ball_goto)
+        .with(flagpole_target_offset(), vec3(0., 0., 1.))
+        .with(flaglin(), 0.05)
+        .with(flaglerp(), 0.01)
+        .with(flaginertia(), 0.1)
+        .spawn();
+    let ball_body = make_ball(0.2)
+        .with(flagpole(), ball_head)
+        .with(flagpole_target_offset(), vec3(0., 0., -1.))
+        .with(flaglin(), 0.02)
+        .with(flaglerp(), 0.1)
+        .with(flaginertia(), 0.1)
+        .spawn();
+    let ball_line = Entity::new()
+        .with(cube(), ())
+        .with(cubeline_width(), 0.1)
+        .spawn();
+    Frame::subscribe(move |_| {
+        entity::mutate_component(ball_head, translation(), |pos| {
+            pos.x += 0.01;
+        });
+        entity::add_components(
+            ball_line,
+            Entity::new()
+                .with(cubeline_a(), pos_of(ball_head))
+                .with(cubeline_b(), pos_of(ball_body)),
+        );
+        if random::<f32>() < 0.05 {
+            entity::add_component(
+                ball_goto,
+                translation(),
+                vec3(random::<f32>() * 10. - 5., random::<f32>() * 10. - 5., 0.),
+            );
+        }
+    });
+}
+
+fn make_ball(radius: f32) -> Entity {
+    Entity::new()
+        .with_merge(Sphere {
+            sphere: (),
+            sphere_radius: radius,
+            sphere_sectors: 12,
+            sphere_stacks: 4,
+        })
+        .with(translation(), vec3(0., 0., 0.))
+}
+
+fn pos_of(entity: EntityId) -> Vec3 {
+    entity::get_component(entity, translation()).unwrap_or(Vec3::ZERO)
 }
